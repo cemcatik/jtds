8df417c0529e72db43c6bfd886230f8f3e85278d
(no author) <(no author)@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2005-03-26 22:11:06 +0000

    This commit was manufactured by cvs2svn to create branch 'J2ME'.
    git-svn-id: svn://svn.code.sf.net/p/jtds/code/branches/J2ME/jtds@771 97a8069c-bbaa-4fa2-9e49-561d8e0b19ef

--- src/SSO/makefile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_INCLUDES=;%JAVA_HOME%\include;%JAVA_HOME%\include\win32
-set INCLUDE=%JAVA_INCLUDES%;%INCLUDE%
-if exist ".\Release/" del /Q .\Release\*.*
-NMAKE /f "ntlmauth.mak" CFG="ntlmauth - Win32 Release"
-
